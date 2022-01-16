d465972b7b6ed106f24e53c98736faea814781af
Tariq Mahmood <tariqmahmood@yandex.com>
2022-01-16 11:59:10 +0500

    1.2

--- app/src/main/java/com/example/applovinmax/MainActivity.java
+++ app/src/main/java/com/example/applovinmax/MainActivity.java
@@ -19,26 +19,35 @@ import com.applovin.mediation.MaxAdViewAdListener;
 import com.applovin.mediation.MaxError;
 import com.applovin.mediation.ads.MaxAdView;
 import com.applovin.mediation.ads.MaxInterstitialAd;
+import com.applovin.mediation.nativeAds.MaxNativeAdListener;
+import com.applovin.mediation.nativeAds.MaxNativeAdLoader;
+import com.applovin.mediation.nativeAds.MaxNativeAdView;
 import com.applovin.sdk.AppLovinSdk;
 import com.applovin.sdk.AppLovinSdkConfiguration;
 
 import java.util.concurrent.TimeUnit;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
 
     private MaxInterstitialAd interstitialAd;
-    private  int retry=0;
+    private int retry = 0;
     private MaxAdView adView;
+
+    private MaxNativeAdLoader nativeAdLoader;
+    private MaxAd nativeAd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         findViewById(R.id.interstital).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(interstitialAd.isReady()){
+                if (interstitialAd.isReady()) {
                     interstitialAd.showAd();
                 }
             }
@@ -51,14 +60,14 @@ public class MainActivity extends AppCompatActivity {
             public void onSdkInitialized(AppLovinSdkConfiguration config) {
                 LoadBannerAd();
                 LoadInterstitalAd();
-
+                createNativeAd();
             }
         });
     }
 
     private void LoadInterstitalAd() {
-        interstitialAd=new MaxInterstitialAd(getResources().getString(R.string.interstitial), this);
-        MaxAdListener adListener= new MaxAdListener() {
+        interstitialAd = new MaxInterstitialAd(getResources().getString(R.string.interstitial), this);
+        MaxAdListener adListener = new MaxAdListener() {
             @Override
             public void onAdLoaded(MaxAd ad) {
 
@@ -82,13 +91,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onAdLoadFailed(String adUnitId, MaxError error) {
                 retry++;
-                long delay = TimeUnit.SECONDS.toMillis((long)Math.pow(2,Math.min(6,retry)));
+                long delay = TimeUnit.SECONDS.toMillis((long) Math.pow(2, Math.min(6, retry)));
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
                         interstitialAd.loadAd();
                     }
-                },delay);
+                }, delay);
 
             }
 
@@ -103,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void LoadBannerAd() {
-        adView= new MaxAdView(getResources().getString(R.string.banner), this);
+        adView = new MaxAdView(getResources().getString(R.string.banner), this);
         adView.setListener(new MaxAdViewAdListener() {
             @Override
             public void onAdExpanded(MaxAd ad) {
@@ -146,14 +155,54 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        int width= ViewGroup.LayoutParams.MATCH_PARENT;
-        int height= getResources().getDimensionPixelSize(R.dimen.banner_height);
-        adView.setLayoutParams(new FrameLayout.LayoutParams(width,height, Gravity.BOTTOM));
+        int width = ViewGroup.LayoutParams.MATCH_PARENT;
+        int height = getResources().getDimensionPixelSize(R.dimen.banner_height);
+        adView.setLayoutParams(new FrameLayout.LayoutParams(width, height, Gravity.BOTTOM));
         adView.setBackgroundColor(Color.WHITE);
 
-        LinearLayout layout= findViewById(R.id.adLayout);
+        LinearLayout layout = findViewById(R.id.adLayout);
         layout.addView(adView);
         adView.loadAd();
 
     }
+
+    private void createNativeAd(){
+        FrameLayout nativeAdContainer = findViewById( R.id.native_ad_layout );
+
+        nativeAdLoader = new MaxNativeAdLoader( getResources().getString(R.string.native_ad), this );
+        nativeAdLoader.setNativeAdListener( new MaxNativeAdListener()
+        {
+            @Override
+            public void onNativeAdLoaded(final MaxNativeAdView nativeAdView, final MaxAd ad)
+            {
+                // Clean up any pre-existing native ad to prevent memory leaks.
+                if ( nativeAd != null )
+                {
+                    nativeAdLoader.destroy( nativeAd );
+                }
+
+                // Save ad for cleanup.
+                nativeAd = ad;
+
+                // Add ad view to view.
+                nativeAdContainer.removeAllViews();
+                nativeAdContainer.addView( nativeAdView );
+            }
+
+            @Override
+            public void onNativeAdLoadFailed(final String adUnitId, final MaxError error)
+            {
+                // We recommend retrying with exponentially higher delays up to a maximum delay
+            }
+
+            @Override
+            public void onNativeAdClicked(final MaxAd ad)
+            {
+                // Optional click callback
+            }
+        } );
+
+        nativeAdLoader.loadAd();
+    }
+
 }
